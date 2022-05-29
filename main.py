bc16a27d148d35b83018bee4209bf4a031f983f3
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-05-29 11:52:31 +0300

    Connecting all webpages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, InputRequired, Email, Length
@@ -13,10 +13,6 @@ class LoginForm(FlaskForm):
     password = PasswordField(label='Password', validators=[InputRequired(), Length(min=8)])
     submit = SubmitField(label='Log In')
 
-    # def validate_name(form, field):
-    #     if len(field.data) > 50:
-    #         raise ValidationError('Name must be less than 50 characters')
-#     'Field must be at list 8 characters long'
 
 app = Flask(__name__)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
@@ -30,11 +26,12 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    # if login_form.validate_on_submit():
-    #     return render_template('success.html')
-    # else:
-    #     return render_template('denied.html')
-    return render_template('login.html', form=login_form)
+    if request.method == "GET":
+        return render_template('login.html', form=login_form)
+    elif request.method == "POST" and login_form.validate_on_submit():
+        return render_template('success.html')
+    else:
+        return render_template('denied.html')
 
 
 @app.route("/success")
