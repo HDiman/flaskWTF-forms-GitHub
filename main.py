9a62f7be85350a3620d0389f225c69e56691d4e4
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-05-29 11:02:54 +0300

    Validating email and size of password

--- main.py
+++ main.py
@@ -30,7 +30,10 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
+    # if login_form.validate_on_submit():
+    #     return render_template('success.html')
+    # else:
+    #     return render_template('denied.html')
     return render_template('login.html', form=login_form)
 
 
